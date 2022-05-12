22ab881531d7c4e21fc76c1c966d6e0f9c839100
chdsz123 <69604714+esme_lopez_r2devpros@users.noreply.github.com>
2022-05-12 02:02:31 -0500

    add second screen and locale english

---
+++ app/src/main/java/com/example/r2devprosproject/UserAddressLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.r2devprosproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserAddressLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_address_layout)
+    }
+}
