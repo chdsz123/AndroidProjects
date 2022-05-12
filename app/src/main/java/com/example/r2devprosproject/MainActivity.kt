6a3608825b5efd2b93110bb779a5ae83201004d3
chdsz123 <69604714+esme_lopez_r2devpros@users.noreply.github.com>
2022-05-12 13:43:22 -0500

    fix implementation versions

--- app/src/main/java/com/example/r2devprosproject/MainActivity.kt
+++ app/src/main/java/com/example/r2devprosproject/MainActivity.kt
@@ -7,14 +7,14 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import com.example.r2devprosproject.databinding.ActivityMainBinding
 import timber.log.Timber
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var addImage: ImageView
-    private lateinit var imageUri: Uri
+    private var image = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Timber.d("MainActivity_TAG: onCreate: ")
@@ -28,11 +28,12 @@ class MainActivity : AppCompatActivity() {
         Timber.d("MainActivity_TAG: onActivityResult: requestCode: $requestCode")
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_CANCELED) {
+            Timber.d("MainActivity_TAG: No image loaded: ")
             // action cancelled
         }
         if (resultCode == RESULT_OK) {
-            val image = data?.data
-            binding.ivAvatar.setImageBitmap(MediaStore.Images.Media.getBitmap(this.contentResolver, image))
+            image = data?.data.toString()
+            binding.ivAvatar.setImageBitmap(MediaStore.Images.Media.getBitmap(this.contentResolver, image.toUri()))
         }
     }
 
@@ -49,21 +50,19 @@ class MainActivity : AppCompatActivity() {
             gallery.type = "image/*"
             gallery.action = Intent.ACTION_GET_CONTENT
 
-            startActivityForResult(Intent.createChooser(gallery, "${getString(R.string.upload_button)}"), PICK_IMAGE)
+            startActivityForResult(Intent.createChooser(gallery, getString(R.string.upload_button)), PICK_IMAGE)
         }
+
         binding.btnAddress.setOnClickListener {
             Timber.d("MainActivity_TAG: Go to Address: ")
 
-//            val image = binding.ivAvatar.drawToBitmap()
-//            val intent = Intent(this@MainActivity, UserAddressLayoutActivity::class.java)
-//            intent.putExtra(CONTACT_NAME, R)
-//            startActivity(intent)
+            val intent = Intent(this@MainActivity, UserAddressLayoutActivity::class.java).apply {
+                putExtras(Bundle().apply {
+                    putString(CONTACT_PHOTO, image)
+                    putString(CONTACT_NAME,"${binding.etName.text} ${binding.etLastName.text}")
+                })
+            }
+            startActivity(intent)
         }
-    }
-
-    companion object {
-        const val CONTACT_NAME = "contact-name"
-        const val CONTACT_PHOTO = "contact-bitmap"
-        const val PICK_IMAGE = 1
     }
 }
---
+++ app/src/main/java/com/example/r2devprosproject/constant.kt
@@ -0,0 +1,5 @@
+package com.example.r2devprosproject
+
+const val CONTACT_NAME = "contact-name"
+const val CONTACT_PHOTO = "contact-bitmap"
+const val PICK_IMAGE = 1
